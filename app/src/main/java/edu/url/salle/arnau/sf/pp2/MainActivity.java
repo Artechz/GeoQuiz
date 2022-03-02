2b47846b28cb5748db410bf3948195197f92dddd
Artechz <arnau.sanz@gmail.com>
2022-03-02 01:38:41 +0100

    Delivery 2 (with challenges) is finished.

--- app/src/main/java/edu/url/salle/arnau/sf/pp2/MainActivity.java
+++ app/src/main/java/edu/url/salle/arnau/sf/pp2/MainActivity.java
@@ -3,28 +3,87 @@ package edu.url.salle.arnau.sf.pp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
+    private TextView mQuestionText;
+
+    private ArrayList<Question> questions = new ArrayList<>();
+    private int questionsIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+        mNextButton = findViewById(R.id.next_button);
+        mPrevButton = findViewById(R.id.previous_button);
+        mQuestionText = findViewById(R.id.question_text);
+
+        String[] qs = getResources().getStringArray(R.array.questions);
+        String[] as = getResources().getStringArray(R.array.answers);
+
+        //instancing Question objects from q&a string arrays in Resources.
+        int i = 0;
+        for (String s : qs) {
+            questions.add(new Question(s, as[i++]));
+        }
+
+        mQuestionText.setText(questions.get(0).getText());
+
         mTrueButton.setOnClickListener(v -> {
-            Toast.makeText(MainActivity.this, R.string.correct_feedback, Toast.LENGTH_SHORT).show();
+            if (questions.get(questionsIndex).isCorrect("TRUE"))
+                Toast.makeText(MainActivity.this, R.string.correct_feedback, Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(MainActivity.this, R.string.wrong_feedback, Toast.LENGTH_SHORT).show();
+            if(!nextQuestion()) Toast.makeText(MainActivity.this, R.string.game_over, Toast.LENGTH_SHORT).show();
         });
 
-        mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(v -> {
-            Toast.makeText(MainActivity.this, R.string.wrong_feedback, Toast.LENGTH_SHORT).show();
+            if (questions.get(questionsIndex).isCorrect("FALSE"))
+                Toast.makeText(MainActivity.this, R.string.correct_feedback, Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(MainActivity.this, R.string.wrong_feedback, Toast.LENGTH_SHORT).show();
+            if(!nextQuestion()) Toast.makeText(MainActivity.this, R.string.game_over, Toast.LENGTH_SHORT).show();
         });
+
+        mNextButton.setOnClickListener(v -> {
+            if(!nextQuestion()) Toast.makeText(MainActivity.this, R.string.game_over, Toast.LENGTH_SHORT).show();
+        });
+
+        mPrevButton.setOnClickListener(v -> previousQuestion());
+
+        mQuestionText.setOnClickListener(v -> nextQuestion());
+
+    }
+
+    private boolean nextQuestion() {
+        if (questionsIndex >= questions.size()-1) return false;
+        else {
+            questionsIndex++;
+            mQuestionText.setText(questions.get(questionsIndex).getText());
+            return true;
+        }
+    }
+
+    private boolean previousQuestion() {
+        if (questionsIndex <= 0) return false;
+        else {
+            questionsIndex--;
+            mQuestionText.setText(questions.get(questionsIndex).getText());
+            return true;
+        }
     }
 }
---
+++ app/src/main/java/edu/url/salle/arnau/sf/pp2/Question.java
@@ -0,0 +1,20 @@
+package edu.url.salle.arnau.sf.pp2;
+
+public class Question {
+
+    private final String text;
+    private final String answer;
+
+    public Question(String text, String answer) {
+        this.text = text;
+        this.answer = answer;
+    }
+
+    public boolean isCorrect(String userAnswer) {
+        return userAnswer.equals(answer);
+    }
+
+    public String getText() {
+        return text;
+    }
+}
